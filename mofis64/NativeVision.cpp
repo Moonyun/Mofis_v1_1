2a670762327856608c1398aaae914013d6f0964a
Moonyun <3463538154@qq.com>
2022-05-10 13:09:15 +0800

    v0510

---
+++ mofis64/NativeVision.cpp
@@ -0,0 +1,397 @@
+#include "NativeVision.h"
+
+NativeVision::NativeVision(LogWindows* pLogWindow)
+{
+	/*m_class_num = in_class_num;
+	m_detect_image_preview_num = in_detect_image_preview_num;*/
+	pStreamSource = NULL;
+	m_celldetect = CellDetect();
+	detect_images = std::vector<std::queue<cv::Mat>>(m_class_num, std::queue<cv::Mat>());
+	m_log_window = pLogWindow;
+}
+
+NativeVision::NativeVision() {
+}
+
+NativeVision::~NativeVision()
+{
+}
+
+//设置相机参数
+bool NativeVision::SetCamParas(int inWidth, int inHeight, int inOffsetX, double inExposureTime, double inAcquisitionFrameRate) {
+	if (pCamera != NULL) {
+		return (
+			modifyCameraOffsetX(pCamera, 0) == 0 &&
+			modifyCameraWidth(pCamera, inWidth) == 0 &&
+			modifyCameraOffsetX(pCamera, inOffsetX) == 0 &&
+			modifyCameraHeight(pCamera, inHeight) == 0 &&
+			modifyCameraExposureTime(pCamera, inExposureTime) == 0 &&
+			modifyCameraAcquisitionFrameRate(pCamera, inAcquisitionFrameRate) == 0
+			);
+	}
+	else {
+		m_log_window->AddLog("no camera when set paras\n");
+		return false;
+	}
+}
+
+//开始捕获图像
+bool NativeVision::StartCapture() {
+	if (pCamera != NULL && pStreamSource == NULL) {
+		std::lock_guard<std::mutex> guard(steamsource_mutex);
+
+		int ret = GENICAM_CreateStreamSource(pCamera, &pStreamSource);
+		if ((ret != 0) || (NULL == pStreamSource)) {
+			m_log_window->AddLog("create stream obj  fail.\r\n");
+			//getchar();
+			return false;
+		}
+		// 开始拉流  
+		// Start grabbing    
+		ret = GENICAM_startGrabbing(pStreamSource);
+		if (ret != 0) {
+			m_log_window->AddLog("StartGrabbing  fail.\n");
+			//注意：需要释放pStreamSource内部对象内存    
+			//Attention: should release pStreamSource internal object before return     
+			pStreamSource->release(pStreamSource);
+			pStreamSource = NULL;
+			//getchar();
+			return false;
+		}
+		m_log_window->AddLog("start capture\n");
+		return true;
+	}
+	else {
+		m_log_window->AddLog("no camera when start capture \n");
+		return false;
+	}
+}
+
+//停止捕获图像
+bool NativeVision::StopCapture() {
+	if (pCamera != NULL && pStreamSource != NULL) {
+		std::lock_guard<std::mutex> guard(steamsource_mutex);
+
+		int ret = GENICAM_stopGrabbing(pStreamSource);
+		if (ret != 0) {
+			m_log_window->AddLog("StopGrabbing  fail.\n");
+			//注意：需要释放pStreamSource内部对象内存    
+			//Attention: should release pStreamSource internal object before return     
+			pStreamSource->release(pStreamSource);
+			//getchar();
+			return false;
+		}
+		pStreamSource->release(pStreamSource);
+		pStreamSource = NULL;
+
+		m_log_window->AddLog("stop capture\n");
+		return true;
+	}
+	else {
+		m_log_window->AddLog("no camear when stop capture \n");
+		return false;
+	}
+}
+
+void NativeVision::SaveImageFromQueue() {
+	std_time time_start = std::chrono::system_clock::now();
+	while (true) {
+		if (!m_bSave || std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - time_start).count() > image_save_interval) {
+			cv::Mat image;
+			std::string path = "";
+			{
+				std::lock_guard<std::mutex> guard(image_mutex);
+				if (!image_queue.empty()) {
+					auto ele = image_queue.front();
+					path = ele.second;
+					image = ele.first;
+					m_log_window->AddLog("get image queue size : %d \n", image_queue.size());
+					image_queue.pop();
+				}
+			}
+			if (!image.empty() && path.size() > 0) {
+				//std::cout << "saving : " << path << std::endl;
+				cv::imwrite(path, image);
+			}
+			image.release();
+			time_start = std::chrono::system_clock::now();
+		}
+	}
+}
+void NativeVision::InsertImageQueue(cv::Mat image, const std::string& imagePath) {
+	{
+		std::lock_guard<std::mutex> guard(image_mutex);
+		if (!image.empty() && imagePath.size() > 0) {
+			image_queue.push(std::make_pair(image, imagePath));
+			m_log_window->AddLog("set image queue size : %d \n", image_queue.size());
+		}
+	}
+}
+
+cv::Mat NativeVision::OperateImageQueue(cv::Mat inImage, bool bInsert) {
+	std::lock_guard<std::mutex> guard(images_mutex);
+	if (bInsert) {
+		//std::cout << "insert " << std::endl;
+		while (!images_queue.empty()) {
+			images_queue.pop();
+		}
+		images_queue.push(inImage);
+	}
+	else {
+		//std::cout << "get  " << std::endl;
+		if (!images_queue.empty()) {
+			return images_queue.front();
+		}
+	}
+	return cv::Mat();
+}
+
+std::vector<cv::Mat> NativeVision::OperateDetectImageQueue(cv::Mat inImage, bool bInsert, int in_class_index) {
+	std::lock_guard<std::mutex> guard(detect_images_mutex);
+	std::vector<cv::Mat> res;
+	if (bInsert) {
+		//std::cout << "insert " << std::endl;
+		detect_images.at(in_class_index).push(inImage);
+		while (detect_images[in_class_index].size() > m_detect_image_preview_num) {
+			detect_images[in_class_index].pop();
+		}
+	}
+	else {
+		//std::cout << "get  " << std::endl;
+		while (!detect_images.at(in_class_index).empty()) {
+			res.push_back(detect_images[in_class_index].back());
+			detect_images[in_class_index].pop();
+		}
+	}
+	return res;
+}
+
+
+
+bool NativeVision::Init()
+{
+	int32_t ret;
+	GENICAM_System* pSystem = NULL;
+	GENICAM_Camera* pCameraList = NULL;
+	GENICAM_AcquisitionControl* pAcquisitionCtrl = NULL;
+	uint32_t cameraCnt = 0;
+	HANDLE threadHandle;
+	unsigned threadID;
+	int cameraIndex = -1;
+	ret = GENICAM_getSystemInstance(&pSystem);
+	if (-1 == ret) {
+		m_log_window->AddLog("pSystem is null.\r\n");
+		getchar();
+		return false;
+	}    // 发现设备     // discover camera     
+	ret = pSystem->discovery(pSystem, &pCameraList, &cameraCnt, typeAll);
+	if (-1 == ret) {
+		m_log_window->AddLog("discovery device fail.\r\n");
+		//getchar();
+		return false;
+	}
+	if (cameraCnt < 1) {
+		m_log_window->AddLog("no camera when init\r\n");
+		//getchar();
+		return false;
+	}
+	// 打印相机基本信息（序号,类型,制造商信息,型号,序列号,用户自定义ID,IP地址）     
+	// Print camera info (Index, Type, Vendor, Model, Serial number, DeviceUserID, IP Address)     
+	//displayDeviceInfo(pCameraList, cameraCnt);
+
+	// 选择需要连接的相机     
+	// Select one camera to connect to      
+	//cameraIndex = selectDevice(cameraCnt);
+	cameraIndex = 0;
+	pCamera = &pCameraList[cameraIndex];
+	// 连接设备     
+	// Connect to camera     
+	ret = GENICAM_connect(pCamera);
+	if (0 != ret) {
+		m_log_window->AddLog("connect camera failed.\n");
+		//getchar();
+		return false;
+	}
+	else {
+		m_log_window->AddLog("connect success ! \n");
+	}
+	return true;
+}
+
+bool NativeVision::BSaveValid() {
+	//std::lock_guard<std::mutex> guard(steamsource_mutex);
+	return pStreamSource == NULL ? false : true;
+}
+
+void createAlphaMat(cv::Mat& mat) // 创建一个图像
+{
+	int randNum = rand() % UCHAR_MAX;
+	for (int i = 0; i < mat.rows; i++)
+	{
+		for (int j = 0; j < mat.cols; j++)
+		{
+			mat.at<uchar>(i, j) = (randNum + 100 * i + j) % UCHAR_MAX;
+		}
+	}
+}
+
+
+
+bool NativeVision::SaveAsync() {
+	return true;
+	return m_bSaveAsync;
+}
+
+//分类算法,后续补充
+int NativeVision::Classify(double inDia) {
+	// to do: classify 
+	if (inDia <= 5) {
+		return 0;
+	}
+	else if (5 < inDia && inDia < 15)
+	{
+		return 1;
+	}
+
+	return 2;
+}
+
+
+//筛选算法
+int NativeVision::Filter(filter_info inSetfilter, const std::string inStore_path, vector<CellInfo> inTotalcellinfo)
+{
+	int s_cell_num = 0;
+	return s_cell_num;
+}
+
+
+//捕获图像
+bool NativeVision::GetImages(int inId, int inSecond, const std::string inName, bool inBGet) {
+
+	bool b_time_to_preview = false;
+	if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - m_time_old).count() > image_preview_interval) {
+		b_time_to_preview = true;
+		m_time_old = std::chrono::system_clock::now();
+	}
+	int32_t ret = 0;
+	uint64_t blockId = 0;
+	GENICAM_Frame* pFrame;
+	std_time ts = std::chrono::system_clock::now();
+	{
+		if (NULL == pStreamSource)
+		{
+			return false;
+		}
+
+		else {
+			if (pStreamSource != NULL) {
+				ret = pStreamSource->getFrame(pStreamSource, &pFrame, 1);
+				if (ret < 0) {
+					m_log_window->AddLog("getFrame  fail.\n");
+					this->StopCapture();
+					Sleep(1);
+					this->StartCapture();
+					return false;
+				}
+
+				ret = pFrame->valid(pFrame);
+				if (ret < 0) {
+					m_log_window->AddLog("frame is invalid!\n");
+					pFrame->release(pFrame);
+					return false;
+				}
+				cv::Mat image = cv::Mat(pFrame->getImageHeight(pFrame),
+					pFrame->getImageWidth(pFrame),
+					CV_8U,
+					(uint8_t*)((pFrame->getImage(pFrame)))
+				);
+
+				if (inBGet) {
+					ImageInfo image_info;
+					image_info.m_image = image.clone();
+					image_info.m_id = inId;
+					image_info.m_name = inName;
+					image_info.m_second = inSecond;
+					m_total_images.push_back(image_info);
+				}
+
+				if (b_time_to_preview) {
+					OperateImageQueue(image.clone(), true);
+				}
+
+				image.release();
+				pFrame->release(pFrame);
+			}
+		}
+	}
+	return true;
+
+}
+
+//原始图像分析
+void NativeVision::AnalyzeImages(const std::string inSampleName, const std::string inRootPath, bool insave_cell, bool save_original_img) {
+	m_total_cells.reserve(10000);
+	int cell_id = 0;
+	std::vector<CellInfo> cellInfos;
+	for (int i = 0; i < m_total_images.size(); ++i) {
+		m_analyze_progress = static_cast<float>(i) / m_total_images.size();
+		cellInfos = m_celldetect.GetResult(m_total_images[i].m_image);
+		for (int j = 0; j < cellInfos.size(); ++j) {
+			CellInfo cell_info = cellInfos[j];
+			cell_info.m_second = m_total_images[i].m_second;
+			cell_info.m_id = cell_id;
+			//cell_info.m_class = Classify(cell_info.m_diameter);
+			cell_info.m_class = 0;
+			cell_info.m_name = inSampleName + "_" + std::to_string(cell_id) + "_" + std::to_string(cell_info.m_class) + "_" + m_total_images[i].m_name + ".bmp";
+			m_total_cells.push_back(cell_info);
+			cell_id++;
+
+			std::filesystem::path root_path = std::filesystem::path(inRootPath) / std::filesystem::path(std::to_string(cell_info.m_class));
+			//std::filesystem::path root_path = std::filesystem::path(inRootPath);
+			if (!std::filesystem::exists(root_path)) {
+				std::filesystem::create_directories(root_path);
+			}
+			std::filesystem::path save_path = root_path / std::filesystem::path(cell_info.m_name);
+			cell_info.m_path = save_path.string();
+			//m_log_window->AddLog("save img in %s \n", save_path.string().c_str());
+			if (insave_cell) {
+				cv::imwrite(save_path.string(), cell_info.m_image);
+			}
+
+		}
+
+		if (save_original_img)
+		{
+			std::filesystem::path img_root_path = std::filesystem::path(inRootPath) / std::filesystem::path(std::string("raw"));
+			if (!std::filesystem::exists(img_root_path)) {
+				std::filesystem::create_directories(img_root_path);
+			}
+			std::filesystem::path save_path = img_root_path / std::filesystem::path(m_total_images[i].m_name + std::to_string(i) + ".bmp");
+			cv::imwrite(save_path.string(), m_total_images[i].m_image);
+		}
+
+	}
+	std::vector<ImageInfo> temp;
+	m_total_images.swap(temp);
+}
+
+float NativeVision::GetAnalyzeProgress() {
+	return m_analyze_progress;
+}
+
+int NativeVision::GetTotalImageSize() {
+	return m_total_images.size();
+}
+
+std::vector<CellInfo> NativeVision::GetTotalCells() {
+	return m_total_cells;
+}
+
+void NativeVision::Clear() {
+	std::vector<CellInfo> temp_cell_infos;
+	std::vector<ImageInfo> temp_images;
+	m_total_cells.swap(temp_cell_infos);
+	m_total_images.swap(temp_images);
+	m_analyze_progress = 0.0;
+
+}
--- mofis64/NativeVision.h
+++ mofis64/NativeVision.h
@@ -1,1 +1,90 @@
-#pragma once
+#ifndef NATIVEVISION_H
+#define NATIVEVISION_H
+
+#include <opencv2/videoio.hpp>
+#include <opencv2/opencv.hpp>
+#include "camera_control.h"
+#include <iostream>
+#include <chrono>
+#include <stdlib.h>
+#include "LogWindows.h"
+#include "CellDetect.h"
+#include <filesystem>
+
+using namespace cv;
+
+using namespace std::literals;
+typedef std::chrono::time_point<std::chrono::system_clock, std::chrono::duration<long long int, std::ratio<1, 1000000000>>> std_time;
+typedef std::unordered_map<int, double> CameraParameters;
+
+struct filter_info {               //É¸Ñ¡Ìõ¼þ
+	float dia_min;
+	float dia_max;
+	float peri_min;
+	float peri_max;
+	float area_min;
+	float area_max;
+	float vol_min;
+	float vol_max;
+	float ell_min;
+	float ell_max;
+};
+
+class NativeVision {
+public:
+	NativeVision();
+	NativeVision(LogWindows* pLogWindow);
+	~NativeVision();
+	bool Init();
+	uint32_t GetDeviceNum();
+	std::vector<CellInfo> GetFrame(bool bSave, bool bSaveAsync, const std::string inSaveName, const std::string inSaveRootPath);
+	bool GetImages(int inId, int inSecond, const std::string inName, bool inBSave);
+	void AnalyzeImages(const std::string inSampleName, const std::string inRootPath, bool save_cell, bool save_original_img);
+	bool SetCamParas(int inWidth, int inHeight, int inOffsetX, double inExposureTime, double inAcquisitionFrameRate);
+	bool StartCapture();
+	bool StopCapture();
+	bool BSaveValid();
+	void SaveImageFromQueue();
+	void InsertImageQueue(cv::Mat image, const std::string& imagePath);
+	bool SaveAsync();
+	cv::Mat OperateImageQueue(cv::Mat inImage, bool bInsert);
+	std::vector<cv::Mat> OperateDetectImageQueue(cv::Mat inImage, bool bInsert, int in_class_index);
+	int Classify(double inDia);
+	int Filter(filter_info inSetfilter, const std::string inStore_path, vector<CellInfo> inTotalcellinfo);
+	float GetAnalyzeProgress();
+	int GetTotalImageSize();
+	std::vector<CellInfo> GetTotalCells();
+	void Clear();
+private:
+	CellDetect m_celldetect;
+	std::string m_save_path;
+	int m_num;
+	GENICAM_StreamSource* pStreamSource;
+	GENICAM_Camera* pCamera = NULL;
+	std::mutex steamsource_mutex;
+	std::mutex image_path_mutex;
+	std::mutex image_mutex;
+	std::mutex images_mutex;
+	std::mutex detect_images_mutex;
+	std::queue<std::string> image_path_queue;
+	std_time m_time_old = std::chrono::system_clock::now();
+	int image_preview_interval = 100;
+	int image_save_interval = 100;
+	std::queue<std::pair<cv::Mat, std::string> > image_queue;
+	std::queue<cv::Mat> images_queue;
+	std::vector<std::queue<cv::Mat>> detect_images;
+	bool m_bSaveAsync = false;
+	bool m_bSave = false;
+	LogWindows* m_log_window;
+	int m_class_num;
+	int m_detect_image_preview_num;
+	std::vector<ImageInfo> m_total_images;
+	std::vector<CellInfo> m_total_cells;
+	float m_analyze_progress = 0.0;
+
+
+
+};
+
+
+#endif
